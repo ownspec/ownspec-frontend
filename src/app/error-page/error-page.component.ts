db38ad8775b0bc5140b412ffb832192357569d5f
lyrold <lyrold.careto@gmail.com>
2017-02-26 16:00:41 +0100

    add error-page component

---
+++ src/app/error-page/error-page.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "error-page",
+  templateUrl: "./error-page.template.html",
+  styleUrls: ['./error-page.scss']
+})
+export class ErrorPageComponent {
+
+  constructor() {
+  }
+}
