eb4334013049eb6e859cc2bfb9c92b5424d46dd7
lyrold <lyrold.careto@gmail.com>
2017-03-07 16:59:56 +0100

    writing mode - remove bottom bar

--- src/assets/js/ckeditor/config.js
+++ src/assets/js/ckeditor/config.js
@@ -5,50 +5,53 @@
 
 
 CKEDITOR.editorConfig = function (config) {
-  config.toolbarGroups = [
-    {name: 'basicstyles', groups: ['basicstyles', 'cleanup']},
-    {name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph']},
-    {name: 'styles', groups: ['styles']},
-    {name: 'insert', groups: ['insert']},
-    {name: 'ownspec', groups: ['ownspec']},
+    config.toolbarGroups = [
+        {name: 'basicstyles', groups: ['basicstyles', 'cleanup']},
+        {name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph']},
+        {name: 'styles', groups: ['styles']},
+        {name: 'insert', groups: ['insert']},
+        {name: 'ownspec', groups: ['ownspec']},
 
-    {name: 'document', groups: ['mode', 'document', 'doctools']},
-    {name: 'clipboard', groups: ['clipboard', 'undo']},
-    {name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing']},
-    //{name: 'forms', groups: ['forms']},
+        {name: 'document', groups: ['mode', 'document', 'doctools']},
+        {name: 'clipboard', groups: ['clipboard', 'undo']},
+        {name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing']},
+        //{name: 'forms', groups: ['forms']},
 
-    {name: 'colors', groups: ['colors']},
-    {name: 'links', groups: ['links']},
-    {name: 'tools', groups: ['tools']},
-    {name: 'others', groups: ['others']},
-    {name: 'about', groups: ['about']}
-  ];
+        {name: 'colors', groups: ['colors']},
+        {name: 'links', groups: ['links']},
+        {name: 'tools', groups: ['tools']},
+        {name: 'others', groups: ['others']},
+        {name: 'about', groups: ['about']}
+    ];
 
-  config.skin = 'moono';
+    config.skin = 'moono';
 
-  config.language = 'en';
-  config.uiColor = '#9AB8F3';
-  config.contentsCss = 'global.css';
-  config.extraPlugins = 'widget,placeholder,sharedspace,divarea,toc,ownspec_compose,ownspec_component,image2';
-  /*
-   config.sharedSpaces = {
-   top: 'someElementId',
-   bottom: 'anotherId'
-   };
-   */
+    config.language = 'en';
+    config.uiColor = '#9AB8F3';
+    config.contentsCss = 'global.css';
+    config.extraPlugins = 'widget,placeholder,sharedspace,divarea,toc,ownspec_compose,ownspec_component,image2';
+    /*
+     config.sharedSpaces = {
+     top: 'someElementId',
+     bottom: 'anotherId'
+     };
+     */
 
-  config.extraAllowedContent = '*[id]';
+    config.extraAllowedContent = '*[id]';
 
-  config.contentsCss = [ 'https://cdn.ckeditor.com/4.6.1/full-all/contents.css', '/styles/ckeditor/style.css' ];
+    config.contentsCss = ['https://cdn.ckeditor.com/4.6.1/full-all/contents.css', '/styles/ckeditor/style.css'];
 
-  config.height = "500px";
+    config.height = "500px";
 
-  config.on = {
-    instanceReady: function () {
-    }
-  };
-  //config.removeButtons = 'Source,Print,Templates,Smiley,Flash,Iframe,ImageButton,Button,Anchor,Maximize,ShowBlocks,About,BidiRtl,BidiLtr,Language,CreateDiv,Blockquote,RemoveFormat,Unlink';
-  //For testing
-  config.removeButtons = 'Print,Templates,Smiley,Flash,Iframe,ImageButton,Button,Anchor,Maximize,ShowBlocks,About,BidiRtl,BidiLtr,Language,CreateDiv,Blockquote,RemoveFormat,Unlink';
+    config.removePlugins = 'elementspath';
+    config.resize_enabled = false;
+
+    config.on = {
+        instanceReady: function () {
+        }
+    };
+    //config.removeButtons = 'Source,Print,Templates,Smiley,Flash,Iframe,ImageButton,Button,Anchor,Maximize,ShowBlocks,About,BidiRtl,BidiLtr,Language,CreateDiv,Blockquote,RemoveFormat,Unlink';
+    //For testing
+    config.removeButtons = 'Print,Templates,Smiley,Flash,Iframe,ImageButton,Button,Anchor,Maximize,ShowBlocks,About,BidiRtl,BidiLtr,Language,CreateDiv,Blockquote,RemoveFormat,Unlink';
 };
 
